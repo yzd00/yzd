ebbf9179af407bbd87b37609be036221721a271b
yzd <1123710573@qq.com>
2016-12-20 11:07:27 +0800

    @

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $signPackage = $jssdk->GetSignPackage();
     <script data-main = "main.js" type="text/javascript" src = "js/require.js"></script>
 </head>
 <body>
-	<div class="home"></div>
+	<!-- <div class="home"></div>
 	<div class = "market"></div>	
 	<div class = "cart"></div>
 	<div class = "mine"></div>
@@ -24,7 +24,8 @@ $signPackage = $jssdk->GetSignPackage();
 		<div class="footer f2"><a href="#market">闪送超市</a></div>
 		<div class="footer f3"><a href="#cart">购物车</a></div>
 		<div class="footer f4"><a href="#mine">我的</a></div>
-	</footer>
+	</footer> -->
+    <input type="button" value="获取位置" id="btn">
 </body>
 <script type="text/javascript">
 	document.documentElement.style.fontSize=innerWidth/10+"px";
@@ -75,5 +76,20 @@ $signPackage = $jssdk->GetSignPackage();
         'openCard'
       ]
   });
+ document.getElementById("btn").onclick = function(){
+     wx.getLocation({
+         success: function (res) {
+             var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+             var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+             var speed = res.speed; // 速度，以米/每秒计
+             var accuracy = res.accuracy; // 位置精度
+            alert("经度，"+longitude+"维度，"+latitude)
+         },
+         cancel: function (res) {
+             alert('用户拒绝授权获取地理位置');
+         }
+     });
+ }
+     
 </script>
 </html>
